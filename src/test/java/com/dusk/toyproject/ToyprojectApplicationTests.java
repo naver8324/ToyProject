d11c351daa9a30fae4d017a143783e4eecbc38b9
[Cloud2] 정주용 <dusk3236@gmail.com>
2024-12-11 07:13:13 +0900

    feat: 프로젝트 시작

---
+++ src/test/java/com/dusk/toyproject/ToyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dusk.toyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7b92ed2d7c5353017bb213858f56777b9e23f7c6
[Cloud2] 정주용 <dusk3236@gmail.com>
2025-10-18 00:33:25 +0900

    feat: divide teams based on players'scores

--- src/main/java/com/dusk/toyproject/domain/player/PlayerService.java
+++ src/main/java/com/dusk/toyproject/domain/player/PlayerService.java
@@ -17,18 +17,39 @@ public class PlayerService {
         return players;
     }
 
-    public List<List<PlayerDto>> divideTeams(int teamCount){
+    public List<List<PlayerDto>> divideTeams(int teamCount) {
         List<List<PlayerDto>> teams = new ArrayList<>();
-
         for (int i = 0; i < teamCount; i++) {
             teams.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < players.size(); i++) {
-            teams.get(i % teamCount).add(players.get(i));
+        players.sort((a, b) -> Integer.compare(b.getScore(), a.getScore()));
+
+        int[] teamScores = new int[teamCount];
+
+        for (PlayerDto player : players) {
+            int targetTeam = 0;
+            int minScore = Integer.MAX_VALUE;
+
+            for (int i = 0; i < teamCount; i++) {
+                int maxSize = players.size() / teamCount + (i < players.size() % teamCount ? 1 : 0);
+
+                if (teams.get(i).size() >= maxSize) {
+                    continue;
+                }
+
+                if (teamScores[i] < minScore) {
+                    minScore = teamScores[i];
+                    targetTeam = i;
+                }
+            }
+
+            teams.get(targetTeam).add(player);
+            teamScores[targetTeam] += player.getScore();
         }
 
         return teams;
     }
+
 
 }
